a3cf2bf153c13832bfbb55525d64d684305c0834
Alex Smith <1763@holbertonschool.com>
2020-06-10 17:17:00 +0000

    adv answer

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - The code
+ *
+ * Return: 0 to end the program
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
