d9dbc491bbce33d925c75e22ca034f1b4b0c24e5
Alex Smith <1763@holbertonschool.com>
2020-06-16 16:25:19 +0000

    reupload

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,5 +6,35 @@
  */
 void print_times_table(int n)
 {
-	_putchar(n);
+	int a, b, c, d, e;
+
+	if (n < 0 || n > 15)
+		return;
+	for (a = 0; a <= n; a++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (b = 1; b <= n; b++)
+		{
+			c = (a * b) / 100;
+			d = ((a * b) % 100) / 10;
+			e = (a * b) % 10;
+			if (c == 0)
+				_putchar(' ');
+			else
+				_putchar(c + '0');
+			if (d == 0)
+				_putchar(' ');
+			else
+				_putchar(d + '0');
+			_putchar(e + '0');
+			if (b != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
