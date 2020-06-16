eba6a177285ce71749feece28e6692a7c74a841d
Alex Smith <1763@holbertonschool.com>
2020-06-16 19:20:39 +0000

    small fix

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,28 +12,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((month == 4 && day > 120))
-	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day - 120, year);
-		return;
-	}
-	if ((month == 6 && day > 181))
-	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day - 181, year);
-		return;
-	}
-	if ((month == 9 && day > 273))
-	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day - 273, year);
-	}
-	if ((month == 11 && day > 334))
-	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day - 334, year);
-		return;
-	}
 	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 	{
-		if (month >= 2 && day >= 60)
+		if ((month == 2 && day >= 60) || month > 2)
 		{
 			day++;
 		}
