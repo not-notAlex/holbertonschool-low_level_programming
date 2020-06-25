019c79c56d97ea5d8aa750fa454184212585300b
Alex Smith <1763@holbertonschool.com>
2020-06-25 19:20:34 +0000

    small fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,6 +11,9 @@ char *_strncat(char *dest, char *src, int n)
 	int size = 0;
 	int i;
 
+	if (n == 0)
+		return (dest);
+
 	while (dest[size])
 		size++;
 	for (i = 0; i < n && src[i] != '\0'; i++)
