99010cba01f1764ed986055c319ed71207b0e60d
Alex Smith <1763@holbertonschool.com>
2020-06-30 01:18:52 +0000

    fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,16 +9,16 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int a, b;
-	int ans = 0;
 
 	for (a = 0; s[a] != '\0'; a++)
 	{
 		for (b = 0; accept[b] != '\0'; b++)
 		{
-			if (s[a] == accept[b])
-				ans++;
+			if (accept[b] == s[a])
+				break;
 		}
+		if (accept[b] == '\0')
+			break;
 	}
-
-	return (ans);
+	return (a);
 }
