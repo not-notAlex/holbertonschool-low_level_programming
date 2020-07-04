566a9f06f36b8d74bdcb305a8144958315982ee9
Alex Smith <1763@holbertonschool.com>
2020-07-04 18:45:48 +0000

    answer

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - The code
+ * @argc: number of inputs
+ * @argv: array of inputs
+ * Return: End all mains with 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 1; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+
+	return (0);
+}
