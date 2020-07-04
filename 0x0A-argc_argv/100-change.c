c9b8a02decfe875df57d127047590f6872c8d4c9
Alex Smith <1763@holbertonschool.com>
2020-07-04 19:49:46 +0000

    adv answer

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - The code
+ * @argc: number of inputs
+ * @argv: array of inputs
+ * Return: End all mains with 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, num, coin = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num = atoi(argv[1]);
+	while (sum + 25 <= num)
+	{
+		sum += 25;
+		coin++;
+	}
+	while (sum + 10 <= num)
+	{
+		sum += 10;
+		coin++;
+	}
+	while (sum + 5 <= num)
+	{
+		sum += 5;
+		coin++;
+	}
+	while (sum + 2 <= num)
+	{
+		sum += 2;
+		coin++;
+	}
+	while (sum + 1 <= num)
+	{
+		sum += 1;
+		coin++;
+	}
+	printf("%d\n", coin);
+	return (0);
+}
