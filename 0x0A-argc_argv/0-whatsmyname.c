5f0534e63800df5dab8b1529f4feaf7d98ec496a
Alex Smith <1763@holbertonschool.com>
2020-07-04 18:23:20 +0000

    testing answer

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - The code
+ * @argc: number of inputs
+ * @argv: array of inputs
+ * Return: End all mains with 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (argc);
+}
