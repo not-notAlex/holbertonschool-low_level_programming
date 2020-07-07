ea84e124646af39c852b6b62d259600e7f48e100
Alex Smith <1763@holbertonschool.com>
2020-07-07 17:33:16 +0000

    small fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,14 +9,15 @@
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int size1 = 0, size2 = 0;
-	unsigned int a, b;
+	unsigned int a = 0, b = 0;
 	char *ar;
 
-	while (s1[size1])
-		size1++;
-
-	while (s2[size2])
-		size2++;
+	if (s1 != NULL)
+		while (s1[size1])
+			size1++;
+	if (s2 != NULL)
+		while (s2[size2])
+			size2++;
 
 	ar = malloc(sizeof(char) * size1 + sizeof(char) * (size2 + 1));
 
