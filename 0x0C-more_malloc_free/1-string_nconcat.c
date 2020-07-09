d76148a9c5710db2e3c55a000e96545281fda725
Alex Smith <1763@holbertonschool.com>
2020-07-09 18:39:37 +0000

    small ifx

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,26 +13,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int a, b;
 	char *ar;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[size1])
-		size1++;
-
-	while (s2[size2] && size2 <= n)
-		size2++;
+	if (s1 != NULL)
+		while (s1[size1])
+			size1++;
+	if (s2 != NULL)
+		while (s2[size2] && size2 <= n)
+			size2++;
 
 	ar = malloc(size1 + size2 + 1);
 	if (ar == NULL)
 		return (NULL);
 
-	for (a = 0; s1[a]; a++)
+	for (a = 0; a < size1; a++)
 		ar[a] = s1[a];
 
-	for (b = 0; s2[b] != '\0' && b < n; b++)
+	for (b = 0; b < size2 && b < n; b++)
 		ar[a + b] = s2[b];
 
 	ar[a + b] = '\0';
