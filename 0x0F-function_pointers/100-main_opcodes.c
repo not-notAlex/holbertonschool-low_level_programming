5b88168c3ee8ced95ef6af95b4da7ad74e0e0f85
Alex Smith <1763@holbertonschool.com>
2020-07-15 21:15:57 +0000

    adv answer

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - The code
+ * @argc: number of inputs
+ * @argv: array of inputs
+ * Return: End all mains with 0
+ */
+int main(int argc, char *argv[])
+{
+	int num;
+	char *codes;
+	int i;
+
+	if (argc < 1)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	num = atoi(argv[1]);
+	if (num < 0)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	codes = malloc(num + 1);
+	if (codes == NULL)
+		return (0);
+	for (i = 0; i < num; i++)
+	{
+		unsigned char *ptr = (main + i);
+
+		codes[i] = ptr;
+	}
+	codes[i] = '\0';
+	for (i = 0; i < num; i++)
+	{
+		printf("%02X", codes[i]);
+		if ((i + 1) != num)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
