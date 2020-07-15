eb1a02f72f30a6fca3a3b69f9aec4d4a45c2fcf3
Alex Smith <1763@holbertonschool.com>
2020-07-15 20:34:52 +0000

    files for task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+
+/**
+ * main - The code
+ * @argc: number of inputs
+ * @argv: array of inputs
+ * Return: End all mains with 0
+ */
+int main(int argc, char *argv[])
+{
+	int n, m, total, a, b;
+	int (*ptr)(int a, int b);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	n = argv[2][0];
+	if (!(n == 43 || n == 45 || n == 47 || n == 42 || n == 37))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	m = atoi(argv[3]);
+	if ((n == 47 && m == 0) || (n == 37 && m ==0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	ptr = get_op_func(argv[2]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	total = (*ptr)(a, b);
+	printf("%d\n", total);
+
+	return (0);
+}
