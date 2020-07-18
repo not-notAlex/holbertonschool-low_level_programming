6c5ef36ddc7bc3530fa3d93dffa3dd6890ace978
Alex Smith <1763@holbertonschool.com>
2020-07-18 17:51:34 +0000

    added test for null

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,11 +10,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
+	char *str;
 
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(valist, char *));
+		str = va_arg(valist, char *);
+		if (str != NULL)
+			printf("%s", str);
 		if (i + 1 != n && separator != NULL)
 			printf("%s", separator);
 	}
