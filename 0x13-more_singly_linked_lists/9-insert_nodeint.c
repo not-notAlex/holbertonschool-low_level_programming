f3fa53236b91b9da4954a05c1fc4c585edf57548
not-notAlex <alex.smith1815@gmail.com>
2020-07-28 07:37:02 +0000

    large index check

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = *head;
 		for (i = 0; i < idx - 1; i++)
+		{
+			if (temp->next == NULL && i < idx - 2)
+				return (NULL);
 			temp = temp->next;
+		}
 		new->next = temp->next;
 		temp->next = new;
 	}
