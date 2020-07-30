3b70505ee1689072ead272c5fdac97236e4c2335
not-notAlex <alex.smith1815@gmail.com>
2020-07-30 21:46:03 +0000

    changed function to not use / or %

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,15 +8,25 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int remainder;
-
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	remainder = n % 2;
-	if (n / 2 != 0)
-		print_binary(n / 2);
-	_putchar(remainder + '0');
+	print_binary_recursion(n);
+}
+
+/**
+ * print_binary_recursion - Prints the binary conversion of a number
+ * @n: number to print
+ *
+ * Return: no return
+ */
+void print_binary_recursion(unsigned long int n)
+{
+	if (n == 0)
+		return;
+
+	print_binary_recursion(n >> 1);
+	_putchar((n & 1) + '0');
 }
