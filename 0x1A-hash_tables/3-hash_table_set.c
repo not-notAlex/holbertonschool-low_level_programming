ee073886d33ff754bce44f4e22c5c3ffd9b5b4e0
not-notAlex <alex.smith1815@gmail.com>
2020-10-02 14:28:41 +0000

    checked for empty string

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,6 +14,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || ht->array == NULL || key == NULL || value == NULL)
 		return (0);
 
+	if (strlen(key) == 0)
+		return (0);
+
 	size = ht->size;
 	index = key_index((unsigned char *)key, size);
 	if (add_node(&ht->array[index], key, value) == NULL)
