08154653fea9cd25c8bceee8515b166d779fa5ac
not-notAlex <alex.smith1815@gmail.com>
2020-11-06 21:24:09 +0000

    now checks for island on edge

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,12 +12,12 @@ def island_perimeter(grid):
     for x in range(0, len(grid)):
         for y in range(0, len(grid[0])):
             if grid[x][y] == 1:
-                if grid[x - 1][y] == 0:
+                if x == 0 or grid[x - 1][y] == 0:
                     total += 1
-                if grid[x + 1][y] == 0:
+                if x == len(grid) - 1 or grid[x + 1][y] == 0:
                     total += 1
-                if grid[x][y + 1] == 0:
+                if y == len(grid[0]) - 1 or grid[x][y + 1] == 0:
                     total += 1
-                if grid[x][y - 1] == 0:
+                if y == 0 or grid[x][y - 1] == 0:
                     total += 1
     return total
